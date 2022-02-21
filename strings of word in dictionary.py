b17e7821471a6f1c02cd906806d85b3358879c30
Masuda Tonima <57611419+matonima@users.noreply.github.com>
2022-02-21 17:56:24 -0500

    Update strings of word in dictionary.py
    this code includes a version that requires only 1 word inputs and a version that can work with normal sentences with spaces in them.

--- strings of word in dictionary.py
+++ strings of word in dictionary.py
@@ -1,65 +1,35 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "name": "Untitled2",
-      "provenance": [],
-      "toc_visible": true,
-      "authorship_tag": "ABX9TyOdpviBCwVuGUXhIvbb+SO/",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/matonima/Simple-codes/blob/main/strings%20of%20word%20in%20dictionary.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "source": [
-        "#given a dictionary of words and a large input string. You have to find out whether the input string can be completely segmented into the words of a given dictionary\n",
-        "def can_segment_string(s, dictionary):\n",
-        "  if (len(s))!=0:\n",
-        "    if s in dictionary:\n",
-        "      y=1\n",
-        "    else:\n",
-        "      y=0\n",
-        "  return (y==1)\n",
-        "dict1=\"apple\",\"pie\",\"not\",\"hi\"\n",
-        "S=\"apple\"\n",
-        "print(can_segment_string(S, dict1))"
-      ],
-      "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "id": "ee2262a9cO_9",
-        "outputId": "01aafb87-eda9-4750-df9f-40966f2cabd7"
-      },
-      "execution_count": 4,
-      "outputs": [
-        {
-          "output_type": "stream",
-          "name": "stdout",
-          "text": [
-            "True\n"
-          ]
-        }
-      ]
-    }
-  ]
-}
+#given a dictionary of words and a large input string. You have to find out whether the input string is present in the dictionary
+#1 word inputs
+def can_segment_string(s, dictionary):
+  if (len(s))!=0:
+    if s in dictionary:
+      y=1
+    else:
+      y=0
+  return (y==1)
+my_dictionary="apple","pie","not","hi"
+my_input="apple"
+print(can_segment_string(my_input,my_dictionary))
+
+##input> 1 word and incudes spaces
+def can_segment_string(s, dictionary):
+    quote = s
+    clean_quote = quote.strip()
+    print(clean_quote)
+    s=clean_quote
+    for i in range(1, len(s) + 1):
+        
+        first = s[0:i]
+        #print(i,first)
+        if first in dictionary:
+            second = s[i:]
+            print(i,first,second)
+            if not second or second in dictionary or can_segment_string(second, dictionary):
+                y= True
+            else:
+                y=False
+            return y
+  
+my_dictionary="apple","pie","not","hi","be","gone","evil","witch","thou","art","not","welcome"
+my_input="apple pie is not witch"
+print(can_segment_string(my_input,my_dictionary))
